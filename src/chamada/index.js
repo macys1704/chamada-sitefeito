de350233aab98c33b19992db14d2cd7d242f75a6
Macys1704 <cherin1704@gmail.com>
2023-09-25 18:20:18 -0300

    :)

--- src/chamada/index.js
+++ src/chamada/index.js
@@ -1,36 +1,61 @@
 import './index.scss';
-import { useState, useEffect } from 'react';
+import { useState } from 'react';
 import axios from 'axios';
 
-function Chamada() {
+export default function Chamada() {
   const [nome, setNome] = useState('');
   const [listaChamada, setListaChamada] = useState([]);
+  
 
   async function Salvar() {
+
+    try {
+
       let inscricao = {
         nome: nome
+        
       };
+
+      
+      setNome('');
+      listar()
+     
 
       let url = 'http://localhost:5000/inserir';
       let resposta = await axios.post(url, inscricao);
-      console.log('Inscrição salva:', resposta.data);
-      setNome('');
+      
+
+    } catch (err) {
+
+    }
+
+
 
   }
 
   async function listar() {
-      let url = 'http://localhost:5000/consulta';
+    let url = 'http://localhost:5000/consulta';
 
-      let resposta = await axios.get(url);
-      setListaChamada(resposta.data);
+    let resposta = await axios.get(url);
+    setListaChamada(resposta.data);
+  }
+
+  async function apagar(id) {
+    let url = `http://localhost:5000/deletar/${id}`
+
+    let resposta = await axios.delete(url, id)
+    console.log(resposta.data.erro)
+    listar()
   }
 
   return (
     <div className="chamada">
       <nav>
+        <h1>Chamada</h1>
         <input value={nome} onChange={(e) => setNome(e.target.value)}></input>
         <button onClick={Salvar}>Salvar</button>
         <button onClick={listar}>Mostrar Lista</button>
+        
         <table>
           <thead>
             <tr>
@@ -39,7 +64,9 @@ function Chamada() {
           <tbody>
             {listaChamada.map((item) => (
               <tr key={item.id_chamada}>
+                <td>{item.id_chamada}</td>
                 <td>{item.nm_nome}</td>
+                <button onClick={() => apagar(item.id_chamada)}>APAGAR</button>
               </tr>
             ))}
           </tbody>
@@ -49,4 +76,4 @@ function Chamada() {
   );
 }
 
-export default Chamada;
+
