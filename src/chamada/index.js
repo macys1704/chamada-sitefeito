33acb0e2d00358179c62d57092adfdbfb1a435ce
Macys1704 <cherin1704@gmail.com>
2023-10-04 23:15:19 -0300

    ainda nao esta finalizado

--- src/chamada/index.js
+++ src/chamada/index.js
@@ -46,40 +46,85 @@ export default function Chamada() {
 
   async function alterar(id) {
     let url = `http://localhost:5000/alterar/${id}`
-    
+
     setNome('');
 
     let resposta = await axios.put(url, { nome: nome })
 
-    
+
   }
 
   return (
-    <div className="chamada">
+    <div className="pagina-autozone">
+
+      <div className='cabecalho'>
+        <img src='/assets/images/imautozone.png' />
+      </div>
+
       <nav>
-        <h1>Chamada</h1>
-        <input value={nome} onChange={(e) => setNome(e.target.value)}></input>
-        <div>
-          <button onClick={Salvar}>Salvar</button>
-          <button onClick={listar}>Mostrar Lista</button>
-        </div>
-        <table>
-          <thead>
-            <tr>
-            </tr>
-          </thead>
-          <tbody>
-            {listaChamada.map((item) => (
-              <tr key={item.id_chamada}>
-                <td>{item.id_chamada}</td>
-                <td>{item.nm_nome}</td>
-                <button onClick={() => apagar(item.id_chamada)}>APAGAR</button>
-                <button onClick={() => alterar(item.id_chamada)}>alterar</button>
-              </tr>
-            ))}
-          </tbody>
-        </table>
+        <h1>Venha para a AutoZone</h1>
       </nav>
+
+      <div className='meio'>
+        <div>
+          <h1>Venha para a AutoZone, aqui você encontra a peça que precisa para seu carro </h1>
+          <p>Adicione sugestões de produtos abaixo</p>
+        </div>
+        <img src='/assets/images/autozonee.jpg' />
+      </div>
+
+      <aside>
+        <div>
+          <h1>Adicione uma peça, ou item</h1>
+
+          <p>Nome do item</p>
+          <div className='input'>
+            <input type='text' value={nome} onChange={e => setNome(e.target.value)} />
+          </div>
+
+          <p>Marca</p>
+          <div className='input'>
+            <input type='text' value={nome} onChange={e => setNome(e.target.value)} />
+          </div>
+
+          <p>Categoria</p>
+          <div className='input'>
+            <input type='text' value={nome} onChange={e => setNome(e.target.value)} />
+          </div>
+
+          <p>Valor</p>
+          <div className='input'>
+            <input type='text' value={nome} onChange={e => setNome(e.target.value)} />
+          </div>
+
+          <p>Modelo</p>
+          <div className='input'>
+            <input type='text' value={nome} onChange={e => setNome(e.target.value)} />
+          </div>
+
+          <button onClick={Salvar}>Salvar</button>
+          <button onClick={listar}>Mostrar lista</button>
+          
+        </div>
+      </aside>
+
+        <table>
+            <thead>
+              <tr>
+              </tr>
+            </thead>
+            <tbody>
+              {listaChamada.map((item) => (
+                <tr key={item.id_chamada}>
+                  <td>{item.id_chamada}</td>
+                  <td>{item.nm_nome}</td>
+                  <button onClick={() => apagar(item.id_chamada)}>APAGAR</button>
+                  <button onClick={() => alterar(item.id_chamada)}>alterar</button>
+                </tr>
+              ))}
+            </tbody>
+          </table>
+
     </div>
   );
 }
